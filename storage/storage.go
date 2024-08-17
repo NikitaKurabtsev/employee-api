5327bf39412e22edf5fa663bb0e3ac5830980c9a
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-17 11:36:33 +0300

    fix employee struct

--- storage/storage.go
+++ storage/storage.go
@@ -1,7 +1,7 @@
 package storage
 
 type Employee struct {
-	ID     int    `json:"id"`
+	Id     int    `json:"id"`
 	Name   string `json:"name"`
 	Sex    string `json:"sex"`
 	Age    int    `json:"age"`
