6fb6c8dbc0336a15046ba1d7d82dda0fb3674cb1
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-17 17:02:19 +0300

    define storage interface and some refactor

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 type Storage interface {
 	Insert(e *Employee)
-	Get()
-	List()
-	Update()
-	Delete()
+	Get(id int) (Employee, error)
+	List() []Employee
+	Update(id int, e *Employee) error
+	Delete(id int) error
 }
 
 func main() {
