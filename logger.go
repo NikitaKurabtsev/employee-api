630e9e47eb8721773dff2b68270f9491eca55002
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-18 11:24:30 +0300

    remove logger to the logger file

---
+++ logger.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+	"os"
+)
+
+func InitLogger() (*slog.Logger, error) {
+	logFile, err := os.OpenFile("employee-api.log",
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	logger := slog.New(slog.NewJSONHandler(logFile, nil))
+	return logger, nil
+}
