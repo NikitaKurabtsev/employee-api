e3db3240154bd6582027beee8df38fb8bac7378e
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-19 11:08:48 +0300

    employee struct replace salary type from string to float32

--- storage.go
+++ storage.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Employee struct {
-	Id     int    `json:"id"`
-	Name   string `json:"name"`
-	Sex    string `json:"sex"`
-	Age    int    `json:"age"`
-	Salary string `json:"salary"`
+	Id     int     `json:"id"`
+	Name   string  `json:"name"`
+	Sex    string  `json:"sex"`
+	Age    int     `json:"age"`
+	Salary float32 `json:"salary"`
 }
 
 type MapMemoryStorage struct {
