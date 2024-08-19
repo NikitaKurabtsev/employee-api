9e1c00632cc2c0da454c5100ece00b9b653e8c41
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-19 19:18:25 +0300

    move validation to the own dir

---
+++ internal/validation/validator.go
@@ -0,0 +1,14 @@
+package validation
+
+import (
+	"fmt"
+
+	"github.com/NikitaKurabtsev/employee-api.git/internal/models"
+)
+
+func ValidateEmployee(e models.Employee) error {
+	if e.Name == "" || e.Age < 0 || e.Salary < 0 {
+		return fmt.Errorf("name and age must be valid")
+	}
+	return nil
+}
