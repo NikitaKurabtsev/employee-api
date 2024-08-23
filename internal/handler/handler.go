7fe08eb7661a3970097e5e180de401771db8789a
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-23 10:21:14 +0300

    huge handler refactor

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,51 +1,58 @@
 package handler
 
 import (
-	"log/slog"
-	"net/http"
-	"strconv"
-
-	"github.com/NikitaKurabtsev/employee-api/internal/errors"
-	"github.com/NikitaKurabtsev/employee-api/internal/httputils"
+	"github.com/NikitaKurabtsev/employee-api/internal/interfaces"
 	"github.com/NikitaKurabtsev/employee-api/internal/models"
-	"github.com/NikitaKurabtsev/employee-api/internal/validation"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 const (
-	// method names
-	createMethodName = "CreateEmployee"
-	updateMethodName = "UpdateEmployee"
-	getAllMethodName = "GetAllEmployees"
-	getMethodName    = "GetEmployee"
-	deleteMethodName = "DeleteEmployee"
-
-	// success messages
-	okCreated = "employee created"
-	okUpdated = "employee updated"
-	okDeleted = "employee deleted"
-	okReaded  = "employee readed"
+	ErrEmployeeInvalidId  = "ID must be a number"
+	ErrInvalidJSON        = "failed to parse JSON"
+	ErrEmployeeUpdate     = "failed to update employee"
+	ErrEmployeeNotFound   = "employee not found"
+	ErrEmployeeValidation = "invalid employee data"
 )
 
-type Repository interface {
-	Insert(e *models.Employee)
-	Get(id int) (models.Employee, error)
-	List() []models.Employee
-	Update(id int, e *models.Employee) error
-	Delete(id int) error
-}
+type (
+	Repository interface {
+		Insert(e *models.Employee)
+		Get(id int) (models.Employee, error)
+		List() []models.Employee
+		Update(id int, e *models.Employee) error
+		Delete(id int) error
+	}
+	Responder interface {
+		RespondWithStatus(c *gin.Context, statusCode int, data any)
+		RespondWithError(c *gin.Context, logger interfaces.Logger, statusCode int, customError string, originalErr error)
+	}
+	Validator interface {
+		ValidateFields(e models.Employee) error
+		ValidateId(c *gin.Context) (int, error)
+	}
+)
 
 type Handler struct {
 	repository Repository
-	logger     *slog.Logger
+	respond    Responder
+	logger     interfaces.Logger
+	validation Validator
 }
 
 // NewHandler returns pointer to the Handler
 // and implements Dependency Injection pattern
-func NewHandler(repository Repository, logger *slog.Logger) *Handler {
+func NewHandler(
+	repository Repository,
+	respond Responder,
+	logger interfaces.Logger,
+	validation Validator,
+) *Handler {
 	return &Handler{
 		repository: repository,
+		respond:    respond,
 		logger:     logger,
+		validation: validation,
 	}
 }
 
@@ -53,171 +60,81 @@ func (h *Handler) CreateEmployee(c *gin.Context) {
 	var employee models.Employee
 
 	if err := c.BindJSON(&employee); err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(createMethodName, errors.ErrInvalidJSON),
-			err,
-		)
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrInvalidJSON, err)
 		return
 	}
 
-	if err := validation.ValidateEmployee(employee); err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(createMethodName, errors.ErrEmployeeValidation),
-			err,
-		)
+	if err := h.validation.ValidateFields(employee); err != nil {
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrEmployeeValidation, err)
 		return
 	}
 
 	h.repository.Insert(&employee)
 
-	httputils.RespondWithStatus(
-		c,
-		h.logger,
-		http.StatusCreated,
-		httputils.OkMessage(createMethodName, okCreated),
-		employee,
-	)
+	h.respond.RespondWithStatus(c, http.StatusCreated, employee)
 }
 
 func (h *Handler) GetAllEmployees(c *gin.Context) {
 	allEmployees := h.repository.List()
 
-	httputils.RespondWithStatus(
-		c,
-		h.logger,
-		http.StatusOK,
-		httputils.OkMessage(getAllMethodName, okReaded),
-		allEmployees,
-	)
+	h.respond.RespondWithStatus(c, http.StatusOK, allEmployees)
 }
 
 func (h *Handler) GetEmployee(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
+	id, err := h.validation.ValidateId(c)
 	if err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(getMethodName, errors.ErrEmployeeInvalidId),
-			err,
-		)
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrEmployeeInvalidId, err)
 		return
 	}
 
 	employee, err := h.repository.Get(id)
 	if err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusNotFound,
-			errors.ErrMessage(getMethodName, errors.ErrEmployeeNotFound),
-			err,
-		)
+		h.respond.RespondWithError(c, h.logger, http.StatusNotFound, ErrEmployeeNotFound, err)
 		return
 	}
 
-	httputils.RespondWithStatus(
-		c,
-		h.logger,
-		http.StatusOK,
-		httputils.OkMessage(getMethodName, okReaded),
-		employee,
-	)
+	h.respond.RespondWithStatus(c, http.StatusOK, employee)
 }
 
 func (h *Handler) UpdateEmployee(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
+	id, err := h.validation.ValidateId(c)
 	if err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(updateMethodName, errors.ErrEmployeeInvalidId),
-			err,
-		)
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrEmployeeInvalidId, err)
 		return
 	}
 
 	var employee models.Employee
-	if err := c.BindJSON(&employee); err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(updateMethodName, errors.ErrInvalidJSON),
-			err,
-		)
+	if err = c.BindJSON(&employee); err != nil {
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrInvalidJSON, err)
 		return
 	}
 	employee.Id = id
 
-	if err := validation.ValidateEmployee(employee); err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(updateMethodName, errors.ErrEmployeeValidation),
-			err,
-		)
+	if err = h.validation.ValidateFields(employee); err != nil {
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrEmployeeValidation, err)
 		return
 	}
 
-	if err := h.repository.Update(id, &employee); err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(updateMethodName, errors.ErrEmployeeUpdate),
-			err,
-		)
+	if err = h.repository.Update(id, &employee); err != nil {
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrEmployeeUpdate, err)
 		return
 	}
 
-	httputils.RespondWithStatus(
-		c,
-		h.logger,
-		http.StatusOK,
-		httputils.OkMessage(updateMethodName, okUpdated),
-		employee,
-	)
+	h.respond.RespondWithStatus(c, http.StatusOK, employee)
 }
 
 func (h *Handler) DeleteEmployee(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
+	id, err := h.validation.ValidateId(c)
 	if err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusBadRequest,
-			errors.ErrMessage(deleteMethodName, errors.ErrEmployeeInvalidId),
-			err,
-		)
+		h.respond.RespondWithError(c, h.logger, http.StatusBadRequest, ErrEmployeeInvalidId, err)
 		return
 	}
 
 	err = h.repository.Delete(id)
 	if err != nil {
-		errors.RespondWithError(
-			c,
-			h.logger,
-			http.StatusNotFound,
-			errors.ErrMessage(deleteMethodName, errors.ErrEmployeeNotFound),
-			err,
-		)
+		h.respond.RespondWithError(c, h.logger, http.StatusNotFound, ErrEmployeeNotFound, err)
 		return
 	}
 
-	httputils.RespondWithStatus(
-		c,
-		h.logger,
-		http.StatusNoContent,
-		httputils.OkMessage(deleteMethodName, okDeleted),
-		0,
-	)
+	h.respond.RespondWithStatus(c, http.StatusNoContent, struct{}{})
 }
