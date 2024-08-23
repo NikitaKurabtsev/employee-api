92d2c9b4682111bd72827f7c9ab3255e51a1d7e4
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-23 10:20:53 +0300

    refactor logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,18 +1,49 @@
 package logger
 
 import (
+	"github.com/gin-gonic/gin"
+	"log"
 	"log/slog"
 	"os"
+	"time"
 )
 
-func InitLogger() (*slog.Logger, error) {
-	logFile, err := os.OpenFile(".employee-api.log",
+func NewFileLogger(fileName string) (*slog.Logger, error) {
+	logFile, err := os.OpenFile(fileName,
 		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		return nil, err
 	}
 
-	logger := slog.New(slog.NewJSONHandler(logFile, nil))
-	logger.Info("Application started")
-	return logger, nil
+	return slog.New(slog.NewJSONHandler(logFile, nil)), nil
+}
+
+func ConsoleLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
+
+		c.Next()
+
+		end := time.Now()
+		latency := end.Sub(start)
+
+		clientIP := c.ClientIP()
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+
+		if raw != "" {
+			path = path + "?" + raw
+		}
+
+		log.Printf("[GIN] %v | %3d | %13v | %15s | %-7s %s\n",
+			end.Format("2006/01/02 - 15:04:05"),
+			statusCode,
+			latency,
+			clientIP,
+			method,
+			path,
+		)
+	}
 }
