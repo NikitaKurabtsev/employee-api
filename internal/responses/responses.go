73e74cd49101cba335547b896d25b9f7dc060d7a
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-23 11:40:39 +0300

    add handler name to the error log message in the respondwitherror

--- internal/responses/responses.go
+++ internal/responses/responses.go
@@ -3,6 +3,8 @@ package responses
 import (
 	"github.com/NikitaKurabtsev/employee-api/internal/interfaces"
 	"github.com/gin-gonic/gin"
+	"runtime"
+	"strings"
 )
 
 type EmployeeResponse struct{}
@@ -30,9 +32,16 @@ func (er EmployeeResponse) RespondWithError(
 	customError string,
 	originalErr error,
 ) {
-	logger.Error(originalErr.Error(), "method", c.Request.Method)
-	c.JSON(statusCode, gin.H{
-		"error":   originalErr.Error(),
-		"message": customError,
-	})
+	var handlerName string
+	// Extracts the handler name from the caller handler.
+	pc, _, _, ok := runtime.Caller(1)
+	if ok {
+		fullName := runtime.FuncForPC(pc).Name()
+		splitName := strings.Split(fullName, ".")
+		handlerName = splitName[len(splitName)-1]
+	}
+
+	logger.Error(originalErr.Error(), "method", c.Request.Method, "handler", handlerName)
+
+	c.JSON(statusCode, gin.H{"error": originalErr.Error(), "message": customError})
 }
