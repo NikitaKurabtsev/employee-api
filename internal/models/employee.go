483362bc3919083891ea3d2f48b20fce4f76f73e
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-31 13:13:36 +0300

    add phone field

--- internal/models/employee.go
+++ internal/models/employee.go
@@ -1,9 +1,10 @@
 package models
 
 type Employee struct {
-	Id     int     `json:"id"`
-	Name   string  `json:"name"`
-	Sex    string  `json:"sex"`
-	Age    int     `json:"age"`
-	Salary float32 `json:"salary"`
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Sex         string  `json:"sex"`
+	Age         int     `json:"age"`
+	Salary      float32 `json:"salary"`
+	PhoneNumber string  `json:"phone_number"`
 }
