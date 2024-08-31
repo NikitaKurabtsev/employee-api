60e7b5fdcbd4da3f96148c0cae66e06c21d1e3b3
nikitaKurabtsev <nick3500hunna@gmail.com>
2024-08-31 13:12:32 +0300

    add phone validation

--- internal/validation/validation.go
+++ internal/validation/validation.go
@@ -3,6 +3,7 @@ package validation
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"regexp"
 	"strconv"
 
 	"github.com/NikitaKurabtsev/employee-api/internal/models"
@@ -15,9 +16,23 @@ func NewEmployeeValidator() *EmployeeValidator {
 }
 
 func (v EmployeeValidator) ValidateFields(e models.Employee) error {
-	if e.Name == "" || e.Age < 0 || e.Salary < 0 {
-		return fmt.Errorf("invalid employee data")
+	pattern := `(\+7|8)?\d{10}$`
+	re := regexp.MustCompile(pattern)
+	isPhoneValid := re.MatchString(e.PhoneNumber)
+
+	if !isPhoneValid {
+		return fmt.Errorf("invalid phone number")
 	}
+	if e.Name == "" {
+		return fmt.Errorf("name cannot be empty")
+	}
+	if e.Age < 0 {
+		return fmt.Errorf("age cannot be negative")
+	}
+	if e.Salary < 0 {
+		return fmt.Errorf("salary cannot be negative")
+	}
+
 	return nil
 }
 
